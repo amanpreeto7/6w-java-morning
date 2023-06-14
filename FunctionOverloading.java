e1d85009b81302eab93b4422f730c4124ce72f8e
Amanpreet Kaur <aman@o7services.com>
2023-06-14 12:33:36 +0530

    Function overloading

--- FunctionOverloading.java
+++ FunctionOverloading.java
@@ -1,10 +1,26 @@
 public class FunctionOverloading {
+    final float pi = 3.14f;
+
     // area is function name
     // square - one
     // rectangle - two
     // circle - one variable float
+    public void area(int a) {
+        System.out.println("Area of square " + (a * a));
+    }
+
+    public void area(int a, int b) {
+        System.out.println("Area of rectangle " + (a * b));
+    }
+
+    public void area(float r) {
+        System.out.println("Area of circle " + ((2 * r) * pi));
+    }
 
     public static void main(String args[]) {
-
+        FunctionOverloading functionOverloading = new FunctionOverloading();
+        functionOverloading.area(2);
+        functionOverloading.area(12, 23);
+        functionOverloading.area(2f);
     }
 }
--- StringBufferBuilderCompare.java
+++ StringBufferBuilderCompare.java
@@ -1,6 +1,15 @@
 public class StringBufferBuilderCompare {
     public static void main(String args[]) {
+        // String, StringBuffer, StringBuilder
+        String a = "Test";
+        String b = new String("qwerty");
+        StringBuffer sb1 = new StringBuffer();
+        sb1.append("b");
+
+        StringBuffer sb2 = new StringBuffer(12);
+
         StringBuffer stringBuffer = new StringBuffer("Testing");
+        System.out.println(stringBuffer.toString().toUpperCase());
         long startTime = System.currentTimeMillis();
         for (int i = 0; i < 5000; i++) {
             stringBuffer.append("Test");
